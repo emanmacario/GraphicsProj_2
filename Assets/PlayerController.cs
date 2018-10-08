5473a5e0b17a252c77963b7caf50b286ce9b8194
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-08 16:01:32 +1100

    Add PlayerController script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float translateForce = -1;
+    public float jumpForce = -1;
+
+    private float DEFAULT_TRANSLATE_FORCE = 1000;
+    private float DEFAULT_JUMP_FORCE = 50;
+    private Transform tf;
+    private Rigidbody rb;
+    private bool isJumping;
+
+    public void Start() {
+        tf = this.transform;
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        translateForce = (translateForce < 0) ? DEFAULT_TRANSLATE_FORCE : translateForce;
+        jumpForce = (jumpForce < 0) ? DEFAULT_JUMP_FORCE : jumpForce;
+    }
+
+    public void Update() {
+
+        float translateMag = translateForce * Time.deltaTime;
+
+        if (Input.GetKey("w") || Input.GetKey(",")) { rb.AddForce(translateMag * Vector3.forward); }
+        if (Input.GetKey("a") || Input.GetKey("a")) { rb.AddForce(translateMag * Vector3.left); }
+        if (Input.GetKey("s") || Input.GetKey("o")) { rb.AddForce(translateMag * Vector3.back); }
+        if (Input.GetKey("d") || Input.GetKey("e")) { rb.AddForce(translateMag * Vector3.right); }
+        if (Input.GetKey("space") && !isJumping) {
+            rb.AddForce(jumpForce * Vector3.up);
+        }
+
+    }
+
+    public void OnCollisionEnter(Collision c) {
+        isJumping = false;
+    }
+
+    public void OnCollisionExit(Collision c) {
+        isJumping = true;
+    }
+
+}
