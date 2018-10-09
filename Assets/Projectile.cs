f80cf3fe205107c627ad189cfde0c89215e99817
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-10 02:36:37 +1100

    Add projectile lifetime

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -4,12 +4,15 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
+    public float lifetime;
     public float speed;
     public Vector3 dir;
 
+    public void setLifetime(float lifetime) { this.lifetime = lifetime; }
     public void setSpeed(float speed) { this.speed = speed; }
     public void setDir(Vector3 dir) { this.dir = Vector3.Normalize(dir); }
 
+    private float lived;
     private Transform tf;
 
     public virtual void Start() {
@@ -17,11 +20,13 @@ public class Projectile : MonoBehaviour {
     }
 
     public void Update() {
+        lived += Time.deltaTime;
+        if (lived > lifetime) Destroy(this.gameObject);
         tf.Translate(Time.deltaTime * speed * dir);
     }
 
     public void OnTriggerEnter(Collider c) {
-        if (c.name.Contains("plat")) Destroy(this.gameObject);
+        /* if (c.name.Contains("plat")) Destroy(this.gameObject); */
     }
 
 }
