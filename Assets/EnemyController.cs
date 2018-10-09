deb8f635b8ef70d9ed4d03b0a767310777ffc434
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-10 05:22:50 +1100

    Add simple Enemy stalking

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -4,20 +4,26 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
+    public float agility;
     public GameObject player;
 
+    private Rigidbody rb;
+
     public void Start() {
+        rb = this.GetComponent<Rigidbody>();
     }
 
     public void Update() {
+        Vector3 self = this.transform.position;
+        Vector3 targ = player.transform.position;
+        Vector3 dir = (targ - self).normalized;
+        rb.AddForce(Time.deltaTime * agility * dir);
     }
 
     public void OnTriggerEnter(Collider c) {
-        /* if (c.name == "JumpPlane") canJump = true; */
     }
 
     public void OnTriggerExit(Collider c) {
-        /* if (c.name == "JumpPlane") canJump = false; */
     }
 
 }
