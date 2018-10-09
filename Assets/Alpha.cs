f71e4aff8cd136a4f713ade6dad1becb08c58fcb
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-09 23:11:13 +1100

    Add radiation type scripts

---
+++ Assets/Alpha.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class Alpha : MonoBehaviour {
+
+    public void Start() {
+        Console.WriteLine("Start Alpha");
+    }
+
+    public void Update() {
+        Console.WriteLine("Update Alpha");
+    }
+
+}
---
+++ Assets/Beta.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class Beta : MonoBehaviour {
+
+    public void Start() {
+        Console.WriteLine("Start Beta");
+    }
+
+    public void Update() {
+        Console.WriteLine("Update Beta");
+    }
+
+}
