cb445ac919bd8574b77d0699fca597a847c28d3f
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-10 02:18:15 +1100

    Switch to automatic fire

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour {
 
     public float translateForce = -1;
     public float jumpForce = -1;
+    public float fireDelay;
     public GameObject typeA;
     public GameObject typeB;
 
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour {
     private Transform tf;
     private Rigidbody rb;
     private bool canJump;
+    private float timeFiredAgo;
 
     private void fire(GameObject type, Vector3 target) {
         GameObject g = Instantiate(type, tf.position, Quaternion.identity);
@@ -27,6 +29,7 @@ public class PlayerController : MonoBehaviour {
         tf = this.transform;
         rb = this.gameObject.GetComponent<Rigidbody>();
         canJump = false;
+        timeFiredAgo = 0;
         if (translateForce < 0) translateForce = DEFAULT_TRANSLATE_FORCE;
         if (jumpForce < 0) jumpForce = DEFAULT_JUMP_FORCE;
     }
@@ -37,13 +40,15 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKey("d") || Input.GetKey("e")) rb.AddForce(translateMag * Vector3.right);
         if (Input.GetKeyDown("space") && canJump) rb.AddForce(jumpForce * Vector3.up);
 
-        bool L = Input.GetMouseButtonDown(0);
-        bool R = Input.GetMouseButtonDown(1);
-        if (L || R) {
+        timeFiredAgo += Time.deltaTime;
+        bool L = Input.GetMouseButton(0);
+        bool R = Input.GetMouseButton(1);
+        if ((L || R) && (timeFiredAgo > fireDelay)) {
             Vector2 mouse = Input.mousePosition;
             Vector3 target = cam.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, -cam.gameObject.transform.position.z));
             if (L) fire(typeA, target);
             if (R) fire(typeB, target);
+            timeFiredAgo = 0;
         }
 
     }
