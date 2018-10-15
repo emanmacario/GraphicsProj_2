7cc38fc06ba58bd2142dd77a3b05090b48b64772
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-15 14:16:43 +1100

    Flip gravity only when player lands

--- Assets/Scripts/GravityAbility.cs
+++ Assets/Scripts/GravityAbility.cs
@@ -4,9 +4,28 @@ using System.Collections;
 
 public class GravityAbility : MonoBehaviour {
 
+    private bool canFlipGravity;
+
+    public voi Start() {
+        canFlipGravity = false;
+    }
+
     public void Update() {
-        if (Input.GetKeyDown("f")) {
+        if (Input.GetKeyDown("f") && canFlipGravity) {
             Physics.gravity *= -1;
+        }
+    }
+
+    public void OnTriggerEnter(Collider c) {
+        String t = c.tag;
+        bool normalGravity = Physics.gravity.y < 0;
+        canFlipGravity = (normalGravity && t.Equals("JumpUp")) || (!normalGravity && t.Equals("JumpDown"));
+    }
+
+    public void OnTriggerExit(Collider c) {
+        String t = c.tag;
+        if (t.Equals("JumpUp") || t.Equals("JumpDown")) {
+            canFlipGravity = false;
         }
     }
 
