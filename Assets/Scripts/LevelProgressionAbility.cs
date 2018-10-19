233c6b271e6a0a49a1a11bfdba52c6a8a84b59ac
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-19 23:24:21 +1100

    Add tmp LevelProgressionAbility

---
+++ Assets/Scripts/LevelProgressionAbility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelProgressionAbility : MonoBehaviour {
+
+    public GameObject levelProgressionObject;
+
+    public void OnTriggerEnter(Collider c) {
+        String t = c.tag;
+        if (t.Equals("NextLevel")) {
+            Console.WriteLine("If player satisfies win condition, move to next level");
+        }
+    }
+
+}
