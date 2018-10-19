01fc5f0dc89153931e70daf5eccf680d66a66bb6
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-19 22:12:00 +1100

    Rename renderer to rend, hide warnings

--- Assets/Scripts/InvisibleBehaviour.cs
+++ Assets/Scripts/InvisibleBehaviour.cs
@@ -9,30 +9,30 @@ public class InvisibleBehaviour : MonoBehaviour {
     public float growFactor = 4;
 
     private LightAbility lightAbility;
-    private MeshRenderer renderer;
+    private MeshRenderer rend;
     private float startRad = 0;
     private float shrinkTime, growTime;
 
     void Start() {
-        renderer = this.gameObject.GetComponent<MeshRenderer>();
+        rend = this.gameObject.GetComponent<MeshRenderer>();
         lightAbility = player.GetComponent<LightAbility>();
     }
 
     // Update is called once per frame
     void Update () {
         if (lightAbility.isActivated()) {
-            renderer.material.SetVector("_invPla", lightAbility.transform.position);
+            rend.material.SetVector("_invPla", lightAbility.transform.position);
             if (growTime < Mathf.PI/2) {
                 shrinkTime = 0;
                 growTime += Time.deltaTime*speed*growFactor;
                 startRad = lightAbility.getRadius() * Mathf.Sin(growTime);
-                renderer.material.SetFloat("_invRad", startRad);
+                rend.material.SetFloat("_invRad", startRad);
             }
         } else if (shrinkTime < Mathf.PI/2) {
             growTime = 0;
-            renderer.material.SetVector("_invPla", lightAbility.transform.position);
+            rend.material.SetVector("_invPla", lightAbility.transform.position);
             shrinkTime += Time.deltaTime*speed;
-            renderer.material.SetFloat("_invRad", startRad * Mathf.Cos(shrinkTime));
+            rend.material.SetFloat("_invRad", startRad * Mathf.Cos(shrinkTime));
         }
 
     }
