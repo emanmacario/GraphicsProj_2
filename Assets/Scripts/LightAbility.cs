f5eb42889d33cc1b9e1e93498c66c85258d7480f
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-20 05:17:14 +1100

    Set invisibility effect to occur periodically, not controlled by player

--- Assets/Scripts/LightAbility.cs
+++ Assets/Scripts/LightAbility.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class LightAbility : MonoBehaviour {
 
     public float radius = 5;
+    public float period = 5;
 
     private Renderer rend;
     private Shader defaultShader;
     private Shader invisibleShader;
     private bool invisible;
+    private float waited;
 
     public bool isActivated() { return invisible; }
     public float getRadius() { return radius; }
@@ -18,10 +20,13 @@ public class LightAbility : MonoBehaviour {
         defaultShader = rend.material.shader;
         invisibleShader = Shader.Find("Unlit/Invisible");
         invisible = false;
+        waited = 0;
     }
 
     public void Update() {
-        if (Input.GetKeyDown("r")) {
+        waited += Time.deltaTime;
+        if (waited > period) {
+            waited = 0;
             rend.material.shader = (invisible) ? defaultShader : invisibleShader;
             invisible = !invisible;
         }
