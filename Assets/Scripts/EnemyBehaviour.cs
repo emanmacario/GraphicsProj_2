2ea10372559ec7ceb8f7093ff23c0a66db210099
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-21 01:21:57 +1100

    Send enemies into wander mode when Player activates light ability

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -7,17 +7,29 @@ public class EnemyBehaviour : MonoBehaviour {
     public float agility = 100;
     public GameObject player;
 
+    private Vector3 lastPos;
     private Rigidbody rb;
+    private LightAbility la;
 
-    public void Start() {
-        rb = this.GetComponent<Rigidbody>();
-    }
-
-    public void Update() {
+    private void UpdateTrackingMode() {
         Vector3 self = this.transform.position;
         Vector3 targ = player.transform.position;
         Vector3 dir = (targ - self).normalized;
         rb.AddForce(Time.deltaTime * agility * dir);
+    }
+
+    private void UpdateWanderMode() {
+    }
+
+    public void Start() {
+        lastPos = Vector3.zero;
+        rb = this.GetComponent<Rigidbody>();
+        la = player.GetComponent<LightAbility>();
+    }
+
+    public void Update() {
+        if (la != null && la.isActivated()) UpdateWanderMode();
+        else UpdateTrackingMode();
     }
 
     public void OnTriggerEnter(Collider c) {
