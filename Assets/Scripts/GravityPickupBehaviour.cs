23de1360a47cc38b055d0afed899f4f21cbdf40e
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-21 07:57:39 +1100

    Notify user and play sound on pickup Gravity ability

--- Assets/Scripts/GravityPickupBehaviour.cs
+++ Assets/Scripts/GravityPickupBehaviour.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 using System;
 using System.Collections;
+using TMPro;
 
 public class GravityPickupBehaviour : MonoBehaviour {
+
+    public GameObject notification;
+
+    private AudioManager am;
+
+    public void Start() {
+        am = GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>();
+    }
 
     public void OnTriggerEnter(Collider c) {
         String t = c.tag;
         if (t.Equals("Player")) {
             c.gameObject.AddComponent<GravityAbility>();
+            notification.GetComponent<MeshRenderer>().enabled = true;
+            am.Play("Pickup");
             Destroy(this.gameObject);
         }
     }
