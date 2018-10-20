926b7e833bd07c8ffc2bfecaf767a14f6bf4e8f5
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-21 06:51:49 +1100

    Destroy projectiles on collision with Platform or Enemy

--- Assets/Scripts/RadABehaviour.cs
+++ Assets/Scripts/RadABehaviour.cs
@@ -4,18 +4,9 @@ using System.Collections;
 
 public class RadABehaviour : ProjectileBehaviour {
 
-    private Rigidbody rb;
-    private MeshRenderer mr;
-
-    public override void Start() {
-        base.Start();
-        rb = this.gameObject.GetComponent<Rigidbody>();
-        mr = this.gameObject.GetComponent<MeshRenderer>();
-    }
-
     public void OnTriggerEnter(Collider c) {
         String t = c.tag;
-        if (t.Equals("PlatV2")) Destroy(this.gameObject);
+        if (t.Equals("PlatV2") || t.Equals("Enemy")) Destroy(this.gameObject);
     }
 
 }
--- Assets/Scripts/RadBBehaviour.cs
+++ Assets/Scripts/RadBBehaviour.cs
@@ -4,4 +4,9 @@ using System.Collections;
 
 public class RadBBehaviour : ProjectileBehaviour {
 
+    public void OnTriggerEnter(Collider c) {
+        String t = c.tag;
+        if (t.Equals("PlatV2") || t.Equals("Enemy")) Destroy(this.gameObject);
+    }
+
 }
