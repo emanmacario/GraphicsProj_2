6d8f3c2f685356c4cd15f33f42f4ef2bb5acad8a
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-20 20:52:34 +1100

    Simplify Gravity enabler

--- Assets/Scripts/GravityPickupBehaviour.cs
+++ Assets/Scripts/GravityPickupBehaviour.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 using System;
 using System.Collections;
 
-public class GravityGiver : MonoBehaviour {
-
-    private bool given = false;
+public class GravityPickupBehaviour : MonoBehaviour {
 
     public void OnTriggerEnter(Collider c) {
         String t = c.tag;
-        if (t.Equals("Player") && !given) {
+        if (t.Equals("Player")) {
             c.gameObject.AddComponent<GravityAbility>();
-            given = true;
+            Destroy(this.gameObject);
         }
     }
 
