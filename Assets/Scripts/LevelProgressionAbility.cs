7898b0b26cc87b48226db1e5e594db009c060cdc
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-21 06:31:56 +1100

    Finish Portal sound before showing next room

--- Assets/Scripts/LevelProgressionAbility.cs
+++ Assets/Scripts/LevelProgressionAbility.cs
@@ -8,17 +8,25 @@ public class LevelProgressionAbility : MonoBehaviour {
 
     public GameObject levelProgressionObject;
 
+    private static const float PORTAL_SOUND_TIME = 1.0f;
+    private static const float FREEZE_SCENE = 0.0f;
     private AudioManager am;
+    private PauseManager pm;
+
+    public void nextScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
     public void Start() {
         am = GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>();
+        pm = new PauseManager(this, PORTAL_SOUND_TIME, FREEZE_SCENE, nextScene);
     }
 
     public void OnTriggerEnter(Collider c) {
         String t = c.tag;
         if (t.Equals("NextLevel")) {
             am.Play("Portal");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            pm.StartPause();
         }
     }
 
