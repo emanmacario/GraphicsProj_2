2442f2f979c9e5a4852999ad280a961213ea07d5
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-21 04:28:57 +1100

    Change some sounds

--- Assets/Scripts/BasicAbility.cs
+++ Assets/Scripts/BasicAbility.cs
@@ -8,10 +8,12 @@ public class BasicAbility : MonoBehaviour {
     public float jumpForce = 60;
 
     private Rigidbody rb;
+    private AudioManager am;
     private bool canJump;
 
     public void Start() {
         rb = this.gameObject.GetComponent<Rigidbody>();
+        am = GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>();
         canJump = false;
     }
 
@@ -19,24 +21,27 @@ public class BasicAbility : MonoBehaviour {
         float translateMag = translateForce * Time.deltaTime;
         if (Input.GetKey("a") || Input.GetKey("a")) rb.AddForce(translateMag * Vector3.left);
         if (Input.GetKey("d") || Input.GetKey("e")) rb.AddForce(translateMag * Vector3.right);
-        if (Input.GetKeyDown("space") && canJump) rb.AddForce(jumpForce * -Physics.gravity);
+        if (Input.GetKeyDown("space") && canJump) {
+            rb.AddForce(jumpForce * -Physics.gravity);
+            am.Play("Jump");
+        }
+    }
+
+    public void OnCollisionEnter(Collision c) {
+        String t = c.gameObject.tag;
+        Console.WriteLine(t);
+        if (t.Equals("PlatV2")) am.Play("LandAfterJump");
     }
 
     public void OnTriggerEnter(Collider c) {
         String t = c.tag;
         bool normalGravity = Physics.gravity.y < 0;
-        if (normalGravity && t.Equals("JumpUp")) {
-            canJump = true;
-        } else if (!normalGravity && t.Equals("JumpDown")) {
-            canJump = true;
-        }
+        if (normalGravity && t.Equals("JumpUp") || !normalGravity && t.Equals("JumpDown")) canJump = true;
     }
 
     public void OnTriggerExit(Collider c) {
         String t = c.tag;
-        if (t.Equals("JumpUp") || t.Equals("JumpDown")) {
-            canJump = false;
-        }
+        if (t.Equals("JumpUp") || t.Equals("JumpDown")) canJump = false;
     }
 
 }
