0147b3e11876e183641972f0dc04fb251c38a263
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-20 18:09:49 +1100

    Remove GravityAbility restrictions

--- Assets/Scripts/GravityAbility.cs
+++ Assets/Scripts/GravityAbility.cs
@@ -4,28 +4,9 @@ using System.Collections;
 
 public class GravityAbility : MonoBehaviour {
 
-    private bool canFlipGravity;
-
-    public void Start() {
-        canFlipGravity = false;
-    }
-
     public void Update() {
-        if (Input.GetKeyDown("f") && canFlipGravity) {
+        if (Input.GetKeyDown("f")) {
             Physics.gravity *= -1;
-        }
-    }
-
-    public void OnTriggerEnter(Collider c) {
-        String t = c.tag;
-        bool normalGravity = Physics.gravity.y < 0;
-        canFlipGravity = (normalGravity && t.Equals("JumpUp")) || (!normalGravity && t.Equals("JumpDown"));
-    }
-
-    public void OnTriggerExit(Collider c) {
-        String t = c.tag;
-        if (t.Equals("JumpUp") || t.Equals("JumpDown")) {
-            canFlipGravity = false;
         }
     }
 
