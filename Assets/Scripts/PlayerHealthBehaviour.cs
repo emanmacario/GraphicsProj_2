c3c790dad38bc5b6243f47512e3074866a10d6ce
rs-1 <rsam1@student.unimelb.edu.au>
2018-10-21 03:06:44 +1100

    Add pseudo-working Particle system with updating emission rate

--- Assets/Scripts/PlayerHealthBehaviour.cs
+++ Assets/Scripts/PlayerHealthBehaviour.cs
@@ -5,10 +5,13 @@ using System.Collections;
 public class PlayerHealthBehaviour : MonoBehaviour {
 
     // Time in secs, player can contact enemy before dying
-    public float health = 8;
+    public float health = 5;
 
+    private float startHealth;
     private Rigidbody rb;
     private MeshRenderer mr;
+    private ParticleSystem ps;
+    private ParticleSystem.EmissionModule em;
 
     private void playerDies() {
         rb.detectCollisions = false;
@@ -16,17 +19,25 @@ public class PlayerHealthBehaviour : MonoBehaviour {
     }
 
     public void Start() {
+        startHealth = health;
         rb = this.gameObject.GetComponent<Rigidbody>();
         mr = this.gameObject.GetComponent<MeshRenderer>();
+        ps = GetComponent<ParticleSystem>();
+        em = ps.emission;
+        ps.Play();
     }
 
     public void Update() {
-        if (health <= 0) playerDies();
+        em = ps.emission;
+        em.rateOverTime = health * 100;
     }
 
     public void OnCollisionStay(Collision c) {
         String t = c.collider.tag;
-        if (t.Equals("Enemy")) health -= Time.deltaTime;
+        if (t.Equals("Enemy")) {
+            health -= Time.deltaTime;
+            if (health <= 0) playerDies();
+        }
     }
 
 }
