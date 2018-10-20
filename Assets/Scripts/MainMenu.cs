ef0c89de66daaa7dd30d6af28bcc10ed3b4dbb1f
EmanMacario <emacario1997@hotmail.com>
2018-10-20 12:50:27 +1100

    Add main menu music

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+	public void Start() {
+		FindObjectOfType<AudioManager>().Play("MainMenuTheme");
+	}
+
 	public void PlayGame() {
 		// Load the next scene according to the build settings scene indexes
 		Invoke("LoadNextScene", 2.0f);
--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -8,7 +8,7 @@ public class Sound {
 	public AudioClip clip;
 	[Range(0f, 1f)]
 	public float volume;
-
+	public bool loop;
 	[HideInInspector]
 	public AudioSource source;
 }
